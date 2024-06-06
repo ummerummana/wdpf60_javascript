e98d5fb0415fe9c0cfae92332a8f0b83d206a668
ar <ar>
2024-06-06 12:46:50 +0600

    06/06/2024

---
+++ Student.js
@@ -0,0 +1,11 @@
+const student_info = () => {
+    let name = "Rummana";
+    let address = "Munich";
+    return `Name: ${name} <br> Address: ${address}`;
+}
+
+let country = "Germany.";
+let capital = " Berlin";
+    
+export default student_info;
+export { capital, country };
